58364b2687b73b36a989e5478e05001a681d5bba
Felipe Pasquali <felipe-pasquali@procergs.rs.gov.br>
2012-09-10 08:40:38 -0300

    Modificado cabecalho do plugin, adicionado bloco copyright e licenca

--- wp-widgetgd_plugin.php
+++ wp-widgetgd_plugin.php
@@ -3,12 +3,27 @@
 Plugin Name: Widget Gabinete Digital
 Plugin URI: http://www.gabinetedigital.rs.gov.br
 Description: Plugin para os widgets utilizados na capa do site do Gabinete Digital.
-Version: 1.0.4
+Version: 1.0.5
 Author: Cristiane | Felipe | Leo 
 Author URI: http://www.procergs.rs.gov.br
 */
 
-//include_once('wp-widgetgd_widgetteste.php');
+/*  Copyright 2012
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License, version 2, as
+published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
 include_once('wp-widgetgd_noticias.php');
 include_once('wp-widgetgd_noticiacompleta.php');
 include_once('wp-widgetgd_fotodia.php');
