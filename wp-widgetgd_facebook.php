34bd1dcaa520986053adc464829474717572d3c9
Leo Andrade <leo-andrade@procergs.rs.gov.br>
2013-10-14 15:41:21 -0300

    Arrumado o fundo do widget e a largura.

--- wp-widgetgd_facebook.php
+++ wp-widgetgd_facebook.php
@@ -10,7 +10,7 @@ class FacebookGD extends WP_Widget
 
   function form($instance)
   {
-    $instance = wp_parse_args( (array) $instance, array( 'titulo' => '', 'fanpage' => '', 'largura' => '', 'altura' => '', 'cor' => 'light', 'faces' => 0, 'header' => 0, 'border' => 0, 'posts' => 0, 'colunas' => '3', 'css_class'=>'' ) );
+    $instance = wp_parse_args( (array) $instance, array( 'titulo' => '', 'fanpage' => '', 'largura' => '', 'altura' => '', 'cor' => '#ffffff', 'faces' => 0, 'header' => 0, 'border' => 0, 'posts' => 0, 'colunas' => '3', 'css_class'=>'' ) );
     $titulo = $instance['titulo'];    
     $fanpage = $instance['fanpage'];
     $largura = $instance['largura'];
@@ -32,7 +32,7 @@ class FacebookGD extends WP_Widget
 
       <p><label for="<?php echo $this->get_field_id('altura'); ?>">Altura do LikeBox: <input class="widefat" id="<?php echo $this->get_field_id('altura'); ?>" name="<?php echo $this->get_field_name('altura'); ?>" type="text" value="<?php echo attribute_escape($altura); ?>"/></label>em branco = default</p>
 
-      <p><label for="<?php echo $this->get_field_id('cor'); ?>">Cor do LikeBox: <input class="widefat" id="<?php echo $this->get_field_id('cor'); ?>" name="<?php echo $this->get_field_name('cor'); ?>" type="text" value="<?php echo attribute_escape($cor); ?>"/></label>light ou dark</p>
+      <p><label for="<?php echo $this->get_field_id('cor'); ?>">Cor do LikeBox: <input class="widefat" id="<?php echo $this->get_field_id('cor'); ?>" name="<?php echo $this->get_field_name('cor'); ?>" type="text" value="<?php echo attribute_escape($cor); ?>"/></label>Colocar hexadecimal. Ex: #ffffff</p>
 
       <p><input type="checkbox" name="<?php echo $this->get_field_name('faces'); ?>" id="<?php echo $this->get_field_id('faces'); ?>" value="1" <?php if ( $faces ) { echo 'checked="checked"'; } ?> /><label for="<?php echo $this->get_field_id('faces'); ?>">&nbsp;Friend Faces</label></p>
 
@@ -81,6 +81,12 @@ class FacebookGD extends WP_Widget
     $colunas = $instance['colunas'];
     $css_class = $instance['css_class'];    
 
+	if (!$largura)
+		if($instance['colunas'] == 1) 
+			$largura = 70;
+		else
+			$largura = 70+($instance['colunas']*100);
+			
 	$txtreturn = "";
 	$txtreturn .= "<li class='span".$instance['colunas']."'>";
     $txtreturn .= '<div id="fb-root"></div><script>(function(d, s, id) {var js, fjs = d.getElementsByTagName(s)[0]; if (d.getElementById(id)) return; js = d.createElement(s); js.id = id; js.src = "//connect.facebook.net/pt_BR/all.js#xfbml=1"; fjs.parentNode.insertBefore(js, fjs);}(document, \'script\', \'facebook-jssdk\'));</script>';
@@ -88,12 +94,13 @@ class FacebookGD extends WP_Widget
     $txtreturn .= ' data-href="https://www.facebook.com/'.$fanpage.'"';
     if($largura != ''){$txtreturn .= ' data-width="'.$largura.'"';}
     if($altura != ''){$txtreturn .= ' data-height="'.$altura.'"';}
-    $txtreturn .= ' data-colorscheme="'.$cor.'"';
+    //$txtreturn .= ' data-colorscheme="'.$cor.'"';
     if($faces){$txtreturn .= ' data-show-faces="true"';}    else{$txtreturn .= ' data-show-faces="false"';}
     if($header){$txtreturn .= ' data-header="true"';}       else{$txtreturn .= ' data-header="false"';}
     if($posts){$txtreturn .= ' data-stream="true"';}        else{$txtreturn .= ' data-stream="false"';}
     if($border){$txtreturn .= ' data-show-border="true"';}  else{$txtreturn .= ' data-show-border="false"';}
     $txtreturn .= '></div>';
+	$txtreturn .= '<style> .fb_iframe_widget {background-color:'.$cor.';}</style>';
 	$txtreturn .= "</li>";
 	
     echo $txtreturn;    
